8794a6bea6a33c0434b0487c26b120ec7e3285ff
Mark Gerken <mgerken86@gmail.com>
2022-07-16 15:26:46 -0600

    Home Page mobile responsive

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,24 +1,24 @@
 import './HomePage.css'
-import wallpaper from '../../images/coding-wallpaper.jpeg'
+import projectsImg from '../../images/mediaProjects.jpg'
 
 export default function HomePage() {
     return (
         <main id="home">
             <div className="homePage page">
+            <h1 className='nameH1'>Hi, I'm Mark</h1>
                 <div id='outerCont'>
-                    <h1 className='nameH1'>Hi, I'm Mark</h1>
                     <div id="middleCont">
                         <div id="innerCont">
                             <img
-                                src={wallpaper}
+                                src={projectsImg}
                                 id="homeImg"
                                 alt="property of https://wallpapersafari.com/w/ZcVzSn" />
                         </div>
                     </div>
-                    <h1 className='typewriter'>
+                </div>
+                <h1 className='typewriter'>
                         I am a 
                     </h1>
-                </div>
             </div>
         </main >
     )
