d5502d09d00888a84d934ea85b9a91444b8e1f0d
Mark Gerken <mgerken86@gmail.com>
2022-07-15 21:54:25 -0600

    Hover effects for nav / footer buttons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,24 +8,28 @@ export default function Footer() {
             <div className='navRoutes2'>
                 <a
                     href="https://www.linkedin.com/in/markgerken86/"
+                    className='footerIcon'
                     target="_blank"
                     rel="noopener noreferrer">
                     <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" icon="linkedin" iconColor="var(--violet)" backgroundColor="var(--lavender)" url="https://some-website.com/my-social-media-url" size="2em" />
                 </a>
                 <a
                     href="https://github.com/mgerken86"
+                    className='footerIcon'
                     target="_blank"
                     rel="noopener noreferrer">
                     <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" icon="github" iconColor="var(--violet)" backgroundColor="var(--lavender)" url="https://some-website.com/my-social-media-url" size="2em" />
                 </a>
                 <a
                     href="https://www.facebook.com/mark.gerken.33/"
+                    className='footerIcon'
                     target="_blank"
                     rel="noopener noreferrer">
                     <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" icon="facebook" iconColor="var(--violet)" backgroundColor="var(--lavender)" url="https://some-website.com/my-social-media-url" size="2em" />
                 </a>
                 <a
                     href="https://www.instagram.com/markgerken/"
+                    className='footerIcon'
                     target="_blank"
                     rel="noopener noreferrer">
                     <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" icon="instagram" iconColor="var(--violet)" backgroundColor="var(--lavender)" url="https://some-website.com/my-social-media-url" size="2em" />
