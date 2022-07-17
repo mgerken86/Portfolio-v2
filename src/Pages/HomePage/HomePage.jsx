6770c15732c6d351858278268d905dd74c177861
Mark Gerken <mgerken86@gmail.com>
2022-07-16 22:42:37 -0600

    Different containers on HomePage. Gradient divs absolute to Image

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -5,22 +5,25 @@ export default function HomePage() {
     return (
         <main id="home">
             <div className="homePage page">
-                
-                <div id='outerCont'>
-                <h1 className='nameH1'>Hi, I'm Mark</h1>
-                    <div id="middleCont">
-                        <div id="innerCont">
-                            <img
-                                src={projectsImg}
-                                id="homeImg"
-                                alt="property of https://wallpapersafari.com/w/ZcVzSn" />
-                        </div>
-                    </div>
+
+
+
+
+                <div id="imgCont">
+                    <div id="topRightGradient"></div>
+                    <div id="rightGradient"></div>
+                    <div id="bottomLeftGradient"></div>
+                    <div id="leftGradient"></div>
+                    <h1 className='nameH1'>Hi, I'm Mark</h1>
+                    <img
+                        src={projectsImg}
+                        id="homeImg"
+                        alt="property of https://wallpapersafari.com/w/ZcVzSn" 
+                    />
                     <h1 className='typewriter'>
-                    I am a
-                </h1>
+                        I am a
+                    </h1>
                 </div>
-                
             </div>
         </main >
     )
