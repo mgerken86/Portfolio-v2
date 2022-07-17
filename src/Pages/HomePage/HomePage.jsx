9ac74d994b6b1d28b358ae4bc000e649e5fe6755
Mark Gerken <mgerken86@gmail.com>
2022-07-16 19:50:48 -0600

    H1s absolute to the middle container

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -5,8 +5,9 @@ export default function HomePage() {
     return (
         <main id="home">
             <div className="homePage page">
-                <h1 className='nameH1'>Hi, I'm Mark</h1>
+                
                 <div id='outerCont'>
+                <h1 className='nameH1'>Hi, I'm Mark</h1>
                     <div id="middleCont">
                         <div id="innerCont">
                             <img
@@ -15,10 +16,11 @@ export default function HomePage() {
                                 alt="property of https://wallpapersafari.com/w/ZcVzSn" />
                         </div>
                     </div>
-                </div>
-                <h1 className='typewriter'>
+                    <h1 className='typewriter'>
                     I am a
                 </h1>
+                </div>
+                
             </div>
         </main >
     )
