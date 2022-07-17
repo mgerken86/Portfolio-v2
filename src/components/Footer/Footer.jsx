76a062f4390b020261fbe78a060b5f7907eaa065
Mark Gerken <mgerken86@gmail.com>
2022-07-16 22:55:02 -0600

    footer anchor tags route to social media sites

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,33 +6,17 @@ export default function Footer() {
     return (
         <nav className='footer'>
             <div className='navRoutes2'>
-                <a
-                    href="https://www.linkedin.com/in/markgerken86/"
-                    className='footerIcon'
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <SocialMediaIconsReact borderColor="var(--pink)" icon="linkedin" iconColor="var(--violet)" backgroundColor="var(--teal)" url="https://some-website.com/my-social-media-url" size="2em" />
+                <a className='footerIcon'>
+                    <SocialMediaIconsReact borderColor="var(--pink)" icon="linkedin" iconColor="var(--violet)" backgroundColor="var(--teal)" url="https://www.linkedin.com/in/markgerken86/" size="2em" />
                 </a>
-                <a
-                    href="https://github.com/mgerken86"
-                    className='footerIcon'
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <SocialMediaIconsReact borderColor="var(--pink)" icon="github" iconColor="var(--violet)" backgroundColor="var(--teal)" url="https://some-website.com/my-social-media-url" size="2em" />
+                <a className='footerIcon'>
+                    <SocialMediaIconsReact borderColor="var(--pink)" icon="github" iconColor="var(--violet)" backgroundColor="var(--teal)" url="https://github.com/mgerken86" size="2em" />
                 </a>
-                <a
-                    href="https://www.facebook.com/mark.gerken.33/"
-                    className='footerIcon'
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <SocialMediaIconsReact borderColor="var(--pink)" icon="facebook" iconColor="var(--violet)" backgroundColor="var(--teal)" url="https://some-website.com/my-social-media-url" size="2em" />
+                <a className='footerIcon'>
+                    <SocialMediaIconsReact borderColor="var(--pink)" icon="facebook" iconColor="var(--violet)" backgroundColor="var(--teal)" url="https://www.facebook.com/mark.gerken.33/" size="2em" />
                 </a>
-                <a
-                    href="https://www.instagram.com/markgerken/"
-                    className='footerIcon'
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <SocialMediaIconsReact borderColor="var(--pink)" icon="instagram" iconColor="var(--violet)" backgroundColor="var(--teal)" url="https://some-website.com/my-social-media-url" size="2em" />
+                <a className='footerIcon'>
+                    <SocialMediaIconsReact borderColor="var(--pink)" icon="instagram" iconColor="var(--violet)" backgroundColor="var(--teal)" url="https://www.instagram.com/markgerken/" size="2em" />
                 </a>
             </div>
             <div id='footerGradient'></div>
