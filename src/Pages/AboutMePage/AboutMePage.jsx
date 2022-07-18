8a201bbb983b75896fc160dab98a95a633297b62
Mark Gerken <mgerken86@gmail.com>
2022-07-17 18:43:20 -0600

    Contact info on About Me Page

--- src/Pages/AboutMePage/AboutMePage.jsx
+++ src/Pages/AboutMePage/AboutMePage.jsx
@@ -1,16 +1,32 @@
 import './AboutMePage.css'
 import markImg from '../../images/mark.png'
+import { IconContext } from 'react-icons'
+import { FaPhoneAlt, FaEnvelope } from 'react-icons/fa'
 
 export default function AboutMePage() {
     return (
         <main id="about">
             <div className="aboutMePage page">
-                <h1>About Me Page</h1>
+                <h1>Some more about me</h1>
                 <div className='markImgContainer'>
+                    <IconContext.Provider
+                        value={{ size: '1em' }}
+                    >
+                        <div className='contactInfo'>
+                        <h3 className='aboutContact'>
+                            <FaPhoneAlt /> (240)750-0263
+                            </h3>
+                            
+                        </div>
+                    
                     <img
                         className='markImg'
                         src={markImg}
                         alt="mark" />
+                    <h3 className="aboutContact pink">
+                                <FaEnvelope /> mgerken86@gmail.com
+                            </h3>
+</IconContext.Provider>
                 </div>
             </div>
         </main>
