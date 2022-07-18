65a9880c581bb8649a6173a7e42f2a40baed7612
Mark Gerken <mgerken86@gmail.com>
2022-07-17 21:55:40 -0600

    skill icons

--- src/Pages/ProjectsPage/ProjectsPage.jsx
+++ src/Pages/ProjectsPage/ProjectsPage.jsx
@@ -4,12 +4,32 @@ import { projectsArr } from '../../utilities/projectData'
 import ProjectCard from '../../components/ProjectCard/ProjectCard'
 
 
+
 export default function ProjectsPage() {
     const [projects, setProjects] = useState(projectsArr)
     return (
         <main id="projects">
             <div className="projectsPage page">
                 <h1>My work</h1>
+                <div className='skillIcons'>
+                    <i class="devicon-javascript-plain" color='white'></i>
+                    <i class="devicon-react-original-wordmark"></i>
+                    <i class="devicon-mongodb-plain-wordmark"></i>
+                    <i class="devicon-express-original"></i>
+                    <i class="devicon-nodejs-plain"></i>                    
+                    <i class="devicon-bootstrap-plain-wordmark"></i>
+                    <i class="devicon-python-plain-wordmark"></i>
+                    <i class="devicon-django-plain"></i>
+                    <i class="devicon-postgresql-plain-wordmark"></i>
+                    <i class="devicon-html5-plain-wordmark"></i>
+                    <i class="devicon-heroku-original-wordmark"></i>
+                    <i class="devicon-github-original-wordmark"></i>
+                    <i class="devicon-css3-plain-wordmark"></i>
+                    <i class="devicon-vscode-plain"></i>
+                    <i class="devicon-typescript-plain"></i>
+                    <i class="devicon-slack-plain"></i>
+                    <i class="devicon-jquery-plain-wordmark"></i>
+                </div>
                 <div className='projectsContainer'>
                     {projects.map((project, idx) => {
                         return <ProjectCard
