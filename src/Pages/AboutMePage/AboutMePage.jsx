da4033cd39067997b639bd6069ee0240787398bf
Mark Gerken <mgerken86@gmail.com>
2022-07-17 18:58:56 -0600

    brand statement

--- src/Pages/AboutMePage/AboutMePage.jsx
+++ src/Pages/AboutMePage/AboutMePage.jsx
@@ -13,20 +13,24 @@ export default function AboutMePage() {
                         value={{ size: '1em' }}
                     >
                         <div className='contactInfo'>
-                        <h3 className='aboutContact'>
-                            <FaPhoneAlt /> (240)750-0263
+                            <h3 className='aboutContact'>
+                                <FaPhoneAlt /> (240)750-0263
                             </h3>
-                            
+
                         </div>
-                    
-                    <img
-                        className='markImg'
-                        src={markImg}
-                        alt="mark" />
-                    <h3 className="aboutContact pink">
-                                <FaEnvelope /> mgerken86@gmail.com
-                            </h3>
-</IconContext.Provider>
+                        <img
+                            className='markImg'
+                            src={markImg}
+                            alt="mark" />
+                        <h3 className="aboutContact pink">
+                            <FaEnvelope /> mgerken86@gmail.com
+                        </h3>
+                    </IconContext.Provider>
+                </div>
+                <div className='brandStatement'>
+                I'm a Software Engineer with a background in Administrative Management, Program Development, and Facilitation. 
+                <br/><br/>My insatiable curiosity keeps me seeking out puzzles, problems, and challenges that force me to find solutions through creativity, critical thinking, perseverance, and collaboration. The speed of evolution in the tech industry excites me, and my passion for knowledge motivates me to adapt my skill-set to match the demands of the industry.
+
                 </div>
             </div>
         </main>
