6b6d8f12cb55c9a9232465d8eb2b4da87960677c
Mark Gerken <mgerken86@gmail.com>
2022-07-20 14:47:55 -0600

    job cards start with large picture to click

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -4,50 +4,68 @@ import { useState } from 'react'
 
 export default function ProjectCard({ project, id }) {
     const [showVideo, setShowVideo] = useState(false)
+    const [showImage, setShowImage] = useState(true)
 
     return (
-        <div className={id % 2 === 0 ? 'projectCardDiv evenProj' : 'projectCardDiv oddProj'}>
-            {!showVideo ? <>
-                <div id="lColumn">
-                    <h2>{project.name}</h2>
-                    <h5>{project.description}</h5>
-                    <br />
-                    <h5>Technologies Used: {project.technologies}</h5>
-                    <div className='linksContainer'>
-                        <a
-                            href={project.link}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <button className='projectBtn'>Deployed Project</button>
-                        </a>
-                        <a
-                            href={project.github}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <img
-                                className='githubIcon'
-                                src={githubIcon} alt=""
-                            />
-                        </a>
-                        <button
-                            onClick={() => setShowVideo(!showVideo)}
-                            className='projectBtn'>Sample Video
-                        </button>
-                    </div>
-                </div>
-                <div id="rColumn">
-                    <img src={project.img} alt="" />
-                </div>
-            </> : <div className='movieContainer'>
-                <button
-                    onClick={() => setShowVideo(!showVideo)}
-                    className='projectBtn'>Go Back
-                </button>
-                <video src={project.video} autoPlay muted></video>
-            </div>
-            }
-        </div>
+        <>
+            {!showImage ?
+                <div
+                    onClick={() => setShowImage(!showImage)}
+                    className={id % 2 === 0 ? 'projectCardDiv evenProj' : 'projectCardDiv oddProj'}>
+
+                    {!showVideo ?
+                        <>
+                            <div id="lColumn">
+                                <h2>{project.name}</h2>
+                                <h5>{project.description}</h5>
+                                <br />
+                                <h5>Technologies Used: {project.technologies}</h5>
+                                <div className='linksContainer'>
+                                    <a
+                                        href={project.link}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <button className='projectBtn'>Deployed Project</button>
+                                    </a>
+                                    <a
+                                        href={project.github}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <img
+                                            className='githubIcon'
+                                            src={githubIcon} alt=""
+                                        />
+                                    </a>
+                                    <button
+                                        onClick={() => setShowVideo(!showVideo)}
+                                        className='projectBtn'>Sample Video
+                                    </button>
+                                </div>
+                            </div>
+                            <div id="rColumn">
+                                <img src={project.img} alt="" />
+                            </div>
+                        </> : <div className='movieContainer'>
+                            <button
+                                onClick={() => setShowVideo(!showVideo)}
+                                className='projectBtn'>Go Back
+                            </button>
+                            <video src={project.video} autoPlay muted></video>
+                        </div>
+                    } </div> :
+                <div 
+                className='projClick'
+                onClick={() => setShowImage(!showImage)}
+                >
+                    <img
+                        src={project.img}
+                        className="largeProjImg"
+                        alt="Project"
+                    />
+                    <h1 id="projH1">Click For More Info!</h1>
+                </div>}
+        </>
     )
 }
