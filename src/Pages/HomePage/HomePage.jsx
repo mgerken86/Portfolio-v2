775fcf89f74839e66f2204a3f91f1913962fab64
Mark Gerken <mgerken86@gmail.com>
2022-08-22 10:50:56 -0600

    title page centered

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -4,7 +4,7 @@ export default function HomePage() {
     return (
         <main id="home">
             <div className="homePage page">
-            <h1 className='nameH1'><span id="teal">Mark Gerken</span></h1>
+                <h1 className='nameH1'>Hi, I'm <span id="teal">Mark</span></h1>
                 <div id="imgCont">
                     <div id="topRightGradient"></div>
                     <div id="rightGradient"></div>
@@ -16,12 +16,11 @@ export default function HomePage() {
                     <div id="rightRightGradient"></div>
                     <img
                         src='/pubImages/flatiron.jpeg'
-                        id="homeImg" 
-                    />
+                        id="homeImg"/>
                 </div>
                 <h1 className='typewriter'>
-                        I am a
-                    </h1>
+                    I am a
+                </h1>
             </div>
         </main >
     )
