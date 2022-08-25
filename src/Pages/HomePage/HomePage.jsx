3391aea668849e0403c8b0226be797cf56708ebb
Mark Gerken <mgerken86@gmail.com>
2022-08-25 17:33:25 -0600

    homepage h1 animated

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,22 @@
 import './HomePage.css'
+import Aos from 'aos'
+import { useEffect } from 'react'
 
 export default function HomePage() {
+
+    //initialize animate-on-scroll
+    useEffect(() => {
+        Aos.init({duration: 3000})
+    }, [])
+
     return (
         <main id="home">
             <div className="homePage page">
-                <h1 className='nameH1'>Hi, I'm <span id="teal">Mark</span></h1>
+                <h1 
+                data-aos='zoom-in'
+                className='nameH1'>
+                    Hi, I'm <span id="teal">Mark</span>
+                    </h1>
                 <div id="imgCont">
                     <div id="topRightGradient"></div>
                     <div id="rightGradient"></div>
@@ -16,7 +28,7 @@ export default function HomePage() {
                     <div id="rightRightGradient"></div>
                     <img
                         src='/pubImages/flatiron.jpeg'
-                        id="homeImg"/>
+                        id="homeImg" />
                 </div>
                 <h1 className='typewriter'>
                     I am a
