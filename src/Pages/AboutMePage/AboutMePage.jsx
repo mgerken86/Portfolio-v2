27cd0bbce138bf23e212e2140224cf715a6d1846
Mark Gerken <mgerken86@gmail.com>
2022-08-26 09:27:30 -0600

    var --bright different color

--- src/Pages/AboutMePage/AboutMePage.jsx
+++ src/Pages/AboutMePage/AboutMePage.jsx
@@ -33,14 +33,18 @@ export default function AboutMePage() {
                 </div>
                 <div className='brandStatement'>
                     "I'm a Software Engineer with a background in Administrative Management, Program Development, and Facilitation.
-                    <br /><br />My insatiable curiosity keeps me seeking out puzzles, problems, and challenges that inspire me to find solutions through creativity, critical thinking, perseverance, and collaboration. The speed of evolution in the tech industry excites me, and my passion for knowledge motivates me to adapt my skill-set to match the demands of the industry."
+                    <br /><br />My insatiable curiosity keeps me seeking out puzzles, problems, and challenges that inspire me to find solutions through creativity, critical thinking, perseverance, and collaboration. The speed of evolution in the tech industry excites me, and my passion for knowledge motivates me to adapt my skill-set to match the demands of the industry.
+                    <br /><br />I love riddles, puzzles, and problem-solving (ask me about escape-room boxes!). I get the same joy from programming that I do from puzzles. As a climber, I enjoy breaking large problems into smaller 'moves' and calculating the most efficient way of getting them done. And most importantly, I am not afraid to fall a thousand times until I find a solution."
                 </div>
                 <h1 id='smallerH1'></h1>
                 <h1 id='smallerH1'></h1>
                 <h1 id='smallerH1'>
                     Want to know what I can bring to your team? <br />
+                    <hr />
                     Have a freelance project for me to assist you with? <br />
+                    <hr />
                     Let's chat!</h1>
+                    
                 <div
                     data-aos='slide-up'
                     className='contactInfo'>
